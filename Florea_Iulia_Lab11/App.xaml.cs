69f704c897e59a529e63ab36a77575129880acf6
iuliaflorea1 <iulia@IULIA_FLOREA>
2024-12-24 00:19:54 +0200

    Add project files.

---
+++ Florea_Iulia_Lab11/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Florea_Iulia_Lab11.Data;
+namespace Florea_Iulia_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Florea_Iulia_Lab11/Data/IRestService.cs
@@ -0,0 +1,18 @@
+﻿using Florea_Iulia_Lab11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Florea_Iulia_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+
+        Task DeleteShopListAsync(int id);
+    }
+}
